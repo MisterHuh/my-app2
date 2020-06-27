362e93cc78171af6cc64de54ec5399a8341686f5
MisterHuh <jaehuh86@yahoo.com>
2020-06-27 12:11:11 -0700

    Updates

---
+++ precache-manifest.9aa5e74380f5230a398e849a4e794aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f6c87f6b1762c234f0ee300e3de3ef",
+    "url": "/MisterHuh/my-app2.git/index.html"
+  },
+  {
+    "revision": "16a36de627be39a88eff",
+    "url": "/MisterHuh/my-app2.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c000be9a922bbbae844",
+    "url": "/MisterHuh/my-app2.git/static/js/2.15527987.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MisterHuh/my-app2.git/static/js/2.15527987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a36de627be39a88eff",
+    "url": "/MisterHuh/my-app2.git/static/js/main.065c1690.chunk.js"
+  },
+  {
+    "revision": "e66ceb68d1ce7ae7d72b",
+    "url": "/MisterHuh/my-app2.git/static/js/runtime-main.62abff03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MisterHuh/my-app2.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
