744a0fd223f18ebd20208c56f42e90d8010289ab
MisterHuh <jaehuh86@yahoo.com>
2020-07-03 16:38:17 -0700

    for retrieving data, actions and reducers set up. need to apply to actual POSt component

---
+++ src/actions/postActions.js
@@ -0,0 +1,32 @@
+import { FETCH_POSTS, NEW_POSTS } from "./types";
+
+
+/* original fetch call from Post.js */
+/* see how the this.setState changes */
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(res => res.json())
+//   .then(data => this.setState({ posts: data }));
+
+
+/* before es6 magic */
+// export function fetchPosts() {
+//   return function(dispatch) {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then(res => res.json())
+//       .then(posts => dispatch({
+//         type:FETCH_POSTS,
+//         payload: posts // the payload will be reduced in the postReducers
+//       }));
+//   }
+// }
+
+/* after es6 magic */
+/* same thing as above, but in a cleanr way */
+export const fetchPosts = () => dispatch => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts => dispatch({
+      type: FETCH_POSTS,
+      payload: posts // the payload will be reduced in the postReducers
+    }));
+}
