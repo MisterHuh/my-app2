fc9acd786617f71916759e93c55994ca821a46f4
MisterHuh <jaehuh86@yahoo.com>
2020-07-03 15:53:02 -0700

    added Provier and store with dummy rootReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux";
 
 import Posts from "./components/Post";
 import Postforms from "./components/Postforms";
 
+import store from "./store";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h5>Welcome to React</h5>
-      </header>
+    <Provider store={store}>
 
-      <Postforms />
-      <hr />
-      <Posts />
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h5>Welcome to React</h5>
+        </header>
+
+        <Postforms />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
