264c8ae31dc291a3db47f89aa621cfc853d77ee6
MisterHuh <jaehuh86@yahoo.com>
2020-07-04 13:21:01 -0700

    added redux dev tools for Chrome

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import combineReducers from "./reducers";
 
@@ -12,7 +12,10 @@ const store = createStore(
   // () => [],
   combineReducers,
   initialState,
-  applyMiddleware(...middleware)
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
